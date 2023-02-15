b79d5518fd1c980d6c36c7f8b3c721a4fad03c67
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2023-02-15 22:22:49 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -1,2 +1,10 @@
 def solution(array):
+    answer=0
+    for a in array:
+        find_7 = str(a).count('7')
+        answer += find_7
+    return answer
+
+
+def solutions(array):
     return str(array).count('7')
