e4d0b6c0634c5c7ab311755380acf3350e5b3219
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2023-02-25 09:59:56 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,14 +1,12 @@
 def solution(s):
     answer = 0
-    sum = 0
     lst = list(s.split(" "))
     for i in range(len(lst)):
         if lst[i] != "Z":
-            sum += int(lst[i])
+            answer += int(lst[i])
         else:
             lst[i] = int(lst[i-1]) * -1
             print(sum)
             print(lst[i])
-            sum+= int(lst[i])
-    answer= sum
+            answer+= int(lst[i])
     return answer
