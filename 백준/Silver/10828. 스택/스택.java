eace2735f9b8b8f064668c3dea602968fbabff17
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-04-08 23:06:23 +0900

    [Silver IV] Title: 스택, Time: 176 ms, Memory: 20152 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+            switch (s[0]) {
+                case "push":
+                    stack.push(Integer.parseInt(s[1]));
+                    break;
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
+}
