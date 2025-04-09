1cb734947bb2e51479caf9f51f178c9c1407d374
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-04-09 23:26:30 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 2.78 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        answer.add(n);
-        for (int i = n; i >= 1; i--){
+        List<Integer> list = new ArrayList<>();
+
+        while (true) {
+            list.add(n);
+            if (n == 1) break;
+
             if (n % 2 == 0) {
-                n = n/2;
-                answer.add(n);
-                
-            } else if (n!=1) {
-                n = 3 * n +1;
-                answer.add(n);
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
             }
         }
-        return answer.stream().mapToInt(i->i).toArray();
+
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
-
-// 자연수 x -> 현재값 x 
-// x가 짝수  / 2
-// x 홀수 = 3*x+1
-// -> 반복 언젠가 x =1
