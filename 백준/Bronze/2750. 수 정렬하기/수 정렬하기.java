7cb7c17ee89d94592d0f828c3290d58197482529
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-04-23 10:39:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 276 ms, Memory: 22144 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] numArr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            numArr[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(numArr);
+
+        for (int num : numArr) {
+            System.out.println(num);
+        }
+    }
+}
