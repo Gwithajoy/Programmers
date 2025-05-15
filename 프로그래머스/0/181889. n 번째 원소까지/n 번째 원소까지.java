073fde3e0a32eac4a5f2d68a30c09dfac69c00d6
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-05-15 12:35:47 +0900

    [level 0] Title: n 번째 원소까지, Time: 3.24 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,10 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return IntStream.range(0, n)
+            .map(i -> num_list[i])
+            .toArray();
+
+    }
+}
