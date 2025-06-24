280b2d533238ed20a2b25277097dd01f4898364a
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-06-24 12:21:31 +0900

    [Silver II] Title: 키로거, Time: 792 ms, Memory: 226964 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -2,40 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        
         int N = Integer.parseInt(br.readLine());
-                
+        
         while (N-- > 0) {
-            char[] keys = br.readLine().toCharArray();
+            char[] key = br.readLine().toCharArray();
             Deque<Character> left = new ArrayDeque<>();
             Deque<Character> right = new ArrayDeque<>();
             
-            for (char k : keys) {
+            for (char k : key) {
                 switch (k) {
-                    case '<':
+                    case '<' :
                         if (!left.isEmpty()) right.push(left.pop());
                         break;
-                    case '>':
+                    case '>' :
                         if (!right.isEmpty()) left.push(right.pop());
                         break;
-                    case '-':
+                    case '-' :
                         if (!left.isEmpty()) left.pop();
                         break;
-                    default :
+                    default:
                         left.push(k);
                         break;
                 }
             }
-            Iterator<Character> it = left.descendingIterator();
-            while (it.hasNext()) {
-                sb.append(it.next());
+            while (!left.isEmpty()) {
+                right.push(left.pop());
             }
             while (!right.isEmpty()) {
                 sb.append(right.pop());
             }
-            sb.append('\n');     
+            sb.append('\n');
         }
         System.out.print(sb);
     }
