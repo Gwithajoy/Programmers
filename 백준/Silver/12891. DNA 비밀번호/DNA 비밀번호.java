2db22a1cce005282735c13444d0d1700ef09c873
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-07-05 21:11:59 +0900

    [Silver II] Title: DNA 비밀번호, Time: 264 ms, Memory: 20220 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] curr = new int[4];
+    static int[] req = new int[4];
+    static int validCount = 0;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        
+        String dna = br.readLine().trim();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            req[i] = Integer.parseInt(st.nextToken());
+            if (req[i] == 0) validCount++;
+        }
+        
+        for (int i = 0; i < P; i++) {
+            add(dna.charAt(i));
+        }
+        
+        int answer = (validCount == 4) ? 1: 0;
+        
+        for (int i = P; i < S; i++) {
+            add (dna.charAt(i));
+            remove (dna.charAt(i - P));
+            
+            if (validCount == 4) answer++;
+        }
+        System.out.println(answer);
+   }
+    
+    private static void add(char c) {
+        int idx = map(c);
+        curr[idx]++;
+        if (curr[idx] == req[idx]) {
+            validCount++;
+        }
+    }
+    
+    private static void remove(char c) {
+        int idx = map(c);
+        if (curr[idx] == req[idx]) {
+            validCount--;
+        }
+        curr[idx]--;
+    }
+    
+    
+    private static int map(char c) {
+        switch (c) {
+            case 'A': return 0;
+            case 'C': return 1;
+            case 'G': return 2;
+            case 'T': return 3;
+            default:  return -1;
+        }
+    }
+}
