f19f24e19949df6c55bad92114e4a215c0544cf9
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-07-26 17:19:45 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1256 ms, Memory: 107956 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(A);
+        
+        for (int n : A) {
+            sb.append(n).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
