50cf2c0b9a6433a14f37ed29de94c0547ddf89e7
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-07-28 12:56:53 +0900

    [Silver II] Title: DFS와 BFS, Time: 188 ms, Memory: 20084 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class Main {
     static int N, M, V;
-    static List<Integer>[] adj;
     static boolean[] visited;
+    static List<Integer>[] adj;
     static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
@@ -18,6 +18,7 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             adj[i] = new ArrayList<>();
         }
+        
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int u = Integer.parseInt(st.nextToken());
@@ -39,32 +40,30 @@ public class Main {
         
         System.out.print(sb);
     }
-        
-        
-       static void dfs(int u) {
-           visited[u] = true;
-           sb.append(u).append(' ');
-           for (int v : adj[u]) {
-               if (! visited[v]) {
-                   dfs(v);
-               }
-           }
-       }
-    
-       static void bfs(int start) {
-           Deque<Integer> q = new ArrayDeque<>();
-           q.offer(start);
-           visited[start] = true;
-           
-           while (!q.isEmpty()) {
-               int u = q.poll();
-               sb.append(u).append(' ');
-               for (int v : adj[u]) {
-                   if (!visited[v]) {
-                       visited[v] = true;
-                       q.offer(v);
-                   }
-               }
-           }
-       }
+    static void dfs(int u) {
+        visited[u] = true;
+        sb.append(u).append(' ');
+        for (int v : adj[u]) {
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
     }
+    static void bfs(int start) {
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visited[start] = true;
+        
+        while(!q.isEmpty()) {
+            int u = q.poll();
+            sb.append(u).append(' ');
+            for (int v: adj[u]) {
+                if (!visited[v]) {
+                visited[v] = true;
+                q.offer(v);
+                
+                }
+            }
+        }
+    }
+}
