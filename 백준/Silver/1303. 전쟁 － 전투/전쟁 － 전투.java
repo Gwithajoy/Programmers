3fc6eabe676e9ba5f6a80bfa142b20ac127bbd99
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-07-29 00:14:21 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 112 ms, Memory: 15352 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -20,33 +20,35 @@ public class Main {
             grid[i] = br.readLine().toCharArray();
         }
         visited = new boolean[M][N];
-      
+        
         long wp = 0;
         long bp = 0;
+        
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
                     int size = dfs(i, j, grid[i][j]);
                     if (grid[i][j] == 'W') {
-                        wp += (long) size * size;
+                        wp += (long)size * size;
                     } else {
-                        bp += (long) size * size;
+                        bp += (long)size * size;
                     }
                 }
             }
         }
         System.out.printf("%d %d\n", wp, bp);
     }
-    
     static int dfs(int x, int y, char color) {
         visited[x][y] = true;
         int count = 1;
         
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d], ny = y + dy[d];
+        for (int d = 0;  d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            
             if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-            if(!visited[nx][ny] && grid[nx][ny] == color) {
-                count+= dfs(nx, ny, color);
+            if (!visited[nx][ny] && grid[nx][ny] == color) {
+                count += dfs(nx, ny, color);
             }
         }
         return count;
