77559f33241ff2c85f48528b173c0d0d675be105
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-08-26 16:05:50 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,16 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static List<Integer>[] adj;
+    static int N;
+    static int M;
     static boolean[] visited;
+    static List<Integer>[] adj;
     static int count = 0;
     
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine().trim());
-        M = Integer.parseInt(br.readLine().trim());
-        
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
         
         adj = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
@@ -30,9 +30,8 @@ public class Main {
             Collections.sort(adj[i]);
         }
         
-        visited = new boolean[N + 1];
+        visited = new boolean[N+1];
         dfs(1);
-        
         System.out.println(count);
     }
     
