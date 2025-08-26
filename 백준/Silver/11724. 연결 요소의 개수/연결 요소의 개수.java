86877aaa87f42f6ac35b30fae0302b80721eda0f
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-08-26 16:18:31 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 604 ms, Memory: 141816 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,39 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N, M;
-    static List<Integer>[] adj;
     static boolean[] visited;
-
+    static List<Integer>[] adj;
+    static int count;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        
         adj = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
-
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             adj[u].add(v);
             adj[v].add(u);
         }
-
-        visited = new boolean[N + 1];
-        int components = 0;
+        
+        visited = new boolean[N+1];
+        count = 0;
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 dfs(i);
-                components++;
+                count++;
             }
         }
-        System.out.println(components);
+        System.out.println(count);
     }
-
     static void dfs(int u) {
         visited[u] = true;
         for (int v : adj[u]) {
