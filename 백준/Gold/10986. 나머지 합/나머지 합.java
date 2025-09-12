a20e08ad1fa2dd0e762181337148296a7e73ed04
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-09-12 15:08:43 +0900

    [Gold III] Title: 나머지 합, Time: 444 ms, Memory: 112912 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,31 +1,31 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        long[] freq = new long[M];
-        long sum = 0;
+        
+        long[] count = new long[M];
         long answer = 0;
         
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
+        long sum = 0;
+        
         for (int i = 0; i < N; i++) {
-            sum = (sum + Long.parseLong(st.nextToken())) % M;
-            freq[(int)sum]++;
+            sum += Long.parseLong(st.nextToken());
+            int r = (int)(sum % M);
+            if (r == 0) answer++;
+            count[r]++;
         }
-        
-        answer += freq[0];
-        
-        for (int r = 0; r < M; r++) {
-            long c = freq[r];
-            if (c > 1) {
-                answer += c * (c - 1) / 2;
-            }
+        for (int i  =0; i < M; i++) {
+            if (count[i] > 1) {
+                answer += (count[i] * (count[i] - 1)) / 2;
+            } 
         }
-        System.out.println(answer);
+         System.out.println(answer);
     }
 }
