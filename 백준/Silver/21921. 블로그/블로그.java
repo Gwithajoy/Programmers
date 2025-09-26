95a63579b99ea7ea68012898c5ab3b0447b0e639
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-09-26 21:39:13 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 35104 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -4,39 +4,40 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
         int N = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int[] A = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
         
-        long sum = 0;
-        for (int i = 0; i < X; i++) {
-            sum += arr[i];
+        int sum = 0;
+        for (int i = 1; i <= K; i++) {
+            sum += A[i];
         }
-        long max = sum;
+        
+        int max = sum;
         int count = 1;
-        
-        for (int i = X; i < N; i++) {
-            sum += arr[i] - arr[i - X];
+        for (int j = K + 1; j <= N; j++) {
+            sum = sum - A[j - K] + A[j];
             
-            if (sum > max) {
+            if (max < sum) {
                 max = sum;
                 count = 1;
-            } else if (sum == max){
+            } else if (max == sum) {
                 count++;
-            }
+            } 
         }
         if (max == 0) {
             System.out.println("SAD");
         } else {
             System.out.println(max);
             System.out.println(count);
-        }       
+        }
+
     }
 }
