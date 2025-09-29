4738df7be3a86597f0c48d1c715bc820d87ca9d3
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-09-29 14:36:26 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,20 +1,16 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine().trim();
-
-        int[] cnt = new int[10];
-        for (int i = 0; i < s.length(); i++) {
-            cnt[s.charAt(i) - '0']++;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int d = 9; d >= 0; d--) {
-            for (int k = 0; k < cnt[d]; k++)
-                sb.append(d);
-        }
-        System.out.println(sb);
-    }
+        String N = br.readLine();
+        
+        char[] num = N.toCharArray();
+        Arrays.sort(num);
+        
+        StringBuilder sb = new StringBuilder(new String(num));
+        System.out.println(sb.reverse().toString());
+        
+    } 
 }
