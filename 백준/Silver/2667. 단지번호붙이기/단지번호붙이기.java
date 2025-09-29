636f0b875c957a065d83e40c8cf605fb61be81a8
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-09-29 22:53:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import java.util.*;
 import java.io.*;
 
@@ -14,7 +36,6 @@ public class Main {
         
         map = new int[N][N];
         visited = new boolean[N][N];
-        
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
             for (int j = 0; j < N; j++) {
@@ -22,34 +43,46 @@ public class Main {
             }
         }
         List<Integer> list = new ArrayList<>();
-        int total  = 0;
-        
+        int total = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (map[i][j] != 0 && !visited[i][j]) {
-                    int cnt = dfs(i, j);
-                    list.add(cnt);
+                    int house = bfs(i, j);
+                    list.add(house);
                     total++;
                 }
             }
         }
         Collections.sort(list);
-        System.out.println(total);
+        System.out.println(total); 
+
         for (int x : list) {
             System.out.println(x);
         }
     }
-    static int dfs(int x, int y) {
+    
+    static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
         visited[x][y] = true;
-        int cnt = 1;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+        int house = 1;
+        
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
             
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] != 0) {
-                cnt += dfs(nx, ny);
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny] && map[nx][ny] == 1) {
+                    q.add(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    house++;
+                }
             }
         }
-        return cnt;
+        return house;
     }
 }
