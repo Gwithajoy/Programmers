3ef972a0222baf713949700cc9800b256536f6ec
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-09-29 16:03:03 +0900

    [Silver V] Title: 나이순 정렬, Time: 572 ms, Memory: 46908 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,34 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static class Member {
         int age;
         String name;
-        Member(int age, String name) {
+        int idx;
+        
+        Member(int age, String name, int idx) {
             this.age = age;
             this.name = name;
+            this.idx = idx;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         
-        List<Member> members = new ArrayList<>(N);
+        List<Member> arr = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            members.add(new Member(age, name));
+            arr.add(new Member(age, name, i));
         }
-        Collections.sort(members, Comparator.comparingInt(m -> m.age));
-        StringBuilder sb = new StringBuilder();
+        arr.sort((s1, s2) -> {
+            if (s1.age != s2.age) return s1.age - s2.age;
+            return s1.idx - s2.idx;           
+        });
         
-        for(Member m : members) {
-            sb.append(m.age)
-                .append(' ')
-                .append(m.name)
-                .append('\n');
+        for (Member m : arr) {
+            sb.append(m.age).append(" ").append(m.name).append('\n');
         }
         System.out.print(sb);
     }
