134e04844021c8e36b7377f6fb15d3fbb90d237f
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-10-02 21:20:58 +0900

    [Silver IV] Title: 수 찾기, Time: 500 ms, Memory: 50760 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -7,19 +7,18 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>(N);
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        HashSet<Integer> set = new HashSet<>(N * 2);
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
         
         int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int x = Integer.parseInt(st.nextToken());
-            sb.append(set.contains(x) ? 1 : 0)
-                .append('\n');
+            sb.append(set.contains(x) ? 1 : 0).append('\n');
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
