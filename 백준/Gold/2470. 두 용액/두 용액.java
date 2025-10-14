75bafb9138514fbb80709b49bbd99e28a8f4e6a9
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-10-14 23:29:33 +0900

    [Gold V] Title: 두 용액, Time: 332 ms, Memory: 28588 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -2,28 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
         int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
         Arrays.sort(arr);
+        
         int left = 0;
         int right = N - 1;
         long best = Long.MAX_VALUE;
-        int answer1 = 0, answer2 = 0;
+        int ans1 = 0, ans2 = 0;
         
         while (left < right) {
-            long sum = (long) arr[left] + arr[right];
+            long sum  = (long) arr[left] + arr[right];
             if (Math.abs(sum) < best) {
                 best = Math.abs(sum);
-                answer1 = arr[left];
-                answer2 = arr[right];
+                ans1 = arr[left];
+                ans2 = arr[right];
             }
             
             if (sum > 0) {
@@ -32,6 +32,6 @@ public class Main {
                 left++;
             }
         }
-        System.out.println(answer1 + " " + answer2);
-    }
+        System.out.println(ans1 + " " + ans2);
+     }
 }
