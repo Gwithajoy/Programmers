af2f70b7507ca6d2bc505e5404933ef0ee9e1c34
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2025-10-17 13:49:29 +0900

    [Silver III] Title: 후위 표기식2, Time: 108 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -1,39 +1,38 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        sc.nextLine();
-        String equation = sc.nextLine();
-        double[] values = new double[num];
-        for (int i = 0; i < num; i++) {
-            values[i] = Double.parseDouble(sc.nextLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        String input = br.readLine().trim();
+        
+        double[] val = new double[N];
+        for (int i = 0; i < N; i++) {
+            val[i] = Double.parseDouble(br.readLine());
         }
-        Deque<Double> stk = new ArrayDeque<>();
-        for (char c : equation.toCharArray()) {
-            if (c >= 'A' && c < 'A' + num) {
-                stk.push(values[c - 'A']);
+        
+        Stack<Double> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            if (ch >= 'A'  && ch <= 'Z') {
+                stack.push(val[ch - 'A']);
             } else {
-                double b = stk.pop();
-                double a = stk.pop();
-                switch (c) {
-                    case '+':
-                        stk.push(a + b);
-                        break;
-                    case '-':
-                        stk.push(a - b);
-                        break;
-                    case '*':
-                        stk.push(a * b);
-                        break;
-                    case '/':
-                        stk.push(a / b);
-                        break;
+                double b = stack.pop();
+                double a = stack.pop();
+                double res = 0.0;
+                
+                switch (ch) {
+                    case '+': res = a + b; break;
+                    case '-': res = a - b; break;
+                    case '*': res = a * b; break;
+                    case '/': res = a / b; break;
                 }
+
+                stack.push(res);
             }
         }
-        System.out.printf("%.2f\n", stk.pop());
-        sc.close();
+        double result = stack.pop();
+        System.out.printf("%.2f", result); 
     }
 }
